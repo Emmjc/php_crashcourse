9a20d2ff3551373a60a6363235e914ec1e10e485
Emmjc <carinomarcus85@gmail.com>
2024-08-11 16:49:28 +0800

    include statement in php using html files

---
+++ includeStatement.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+        include "header.html"   // purpose nito is para i lagay ung .html na file sa file na ito, without doing the code sa mismong file, tatawagin mo lng.
+    ?>
+    
+    <p> This is a sample article. </p>
+
+    <?php 
+        include "footer.html"
+    ?>
+</body>
+</html>
